a2ff2d326635bc5ac5a6ed9ffbe4cbf52d7e1fa4
Sauvik Dolui <sauvik.dolui@innofied.com>
2014-10-09 13:22:53 +0530

    Initial Commit

---
+++ Cocos2dParallxScratch/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Cocos2dParallxScratch
+//
+//  Created by Sauvik Dolui on 10/9/14.
+//  Copyright Innofied Solution Pvt. Ltd. 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ Cocos2dParallxScratch/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Cocos2dParallxScratch
+//
+//  Created by Sauvik Dolui on 10/9/14.
+//  Copyright Innofied Solution Pvt. Ltd. 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
