205e345e4dd796cd56b1c4ca6f7b14d4c473af42
Sauvik Dolui <sauvik.dolui@innofied.com>
2014-10-09 14:58:01 +0530

    Initial Project

---
+++ Cocos2dParallxScratch/ParallaxScene.h
@@ -0,0 +1,16 @@
+//
+//  ParallaxScene.h
+//  Cocos2dParallxScratch
+//
+//  Created by Sauvik Dolui on 10/9/14.
+//  Copyright 2014 Innofied Solution Pvt. Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface ParallaxScene : CCScene {
+    
+}
+
+@end
---
+++ Cocos2dParallxScratch/ParallaxScene.m
@@ -0,0 +1,14 @@
+//
+//  ParallaxScene.m
+//  Cocos2dParallxScratch
+//
+//  Created by Sauvik Dolui on 10/9/14.
+//  Copyright 2014 Innofied Solution Pvt. Ltd. All rights reserved.
+//
+
+#import "ParallaxScene.h"
+
+
+@implementation ParallaxScene
+
+@end
